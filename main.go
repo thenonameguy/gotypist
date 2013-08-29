e8898636446aa02d6be9fd35cf267dcf3ab11250
Krisztián Szabó <thenonameguy24@gmail.com>
2013-08-29 23:37:09 +0200

    Made new design work on dev systems

--- main.go
+++ main.go
@@ -15,6 +15,11 @@ func SocketServer(ws *websocket.Conn){
 
 var port string
 
+func serveSingle(pattern string, filename string) {
+  http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, filename)
+  })
+}
 func main() {
 	flag.StringVar(&port, "port", ":8080", "serving port")
 	flag.Parse()
@@ -22,6 +27,8 @@ func main() {
   http.HandleFunc("/index.html",IndexHandler)
 	http.HandleFunc("/race", RaceHandler)
 	http.Handle("/socket", websocket.Handler(SocketServer))
+  serveSingle("/scripts.js","web/scripts.js")
+  serveSingle("/style.css","web/style.css")
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
